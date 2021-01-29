b9e2a6402083edcccd721a04ce152a868be7a4c0
Daniel Ferraz <Daniel0ferraz@outlook.com>
2021-01-29 11:47:14 -0300

    finalizando aplicação consumindo api do pokeApi

--- js/script.js
+++ js/script.js
@@ -1,44 +1,124 @@
-const cards = document.querySelector('.cards')
-const card = document.querySelector('.card');
+const types = [
+    'fire',
+    'grass',
+    'electric',
+    'water',
+    'ground',
+    'rock',
+    'fairy',
+    'poison',
+    'bug',
+    'dragon',
+    'psychic',
+    'flying',
+    'fighting',
+    'normal',
+];
+const POKEMON_COUNT = 12;
 
-for(let i = 0; i < 30; i++) {
-   const clone = card.cloneNode(true);
-   cards.appendChild(clone)
+const cardHTML = `
+  <div class="card id="card-{id}">
+  <div class="title">
+      <h2>{name}</h2>
+      <small># {id}</small>
+  </div>
+
+  <div class="img bg-{type}">
+      <img src="https://pokeres.bastionbot.org/images/pokemon/{id}.png" alt="{name}">
+  </div>
+
+  <div class="type {type}">
+      <p>{type}</p>
+  </div>
+
+  <button class="favorites" data-id={id}>
+      <div class="heart">
+      </div>
+  </button>
+  `;
+
+  const cards = document.querySelector('.cards');
+
+const getType = (data) => {
+    const apitypes = data.map((type) => type.type.name);
+    const type = types.find(type => apitypes.indexOf(type) > -1);
+    return type;
 }
 
-const colors = {
-fire: '#e4604d',
-grass: '#9dd465',
-electric:'#f9e45f',
-water: '#6a83d6',
-ground: '#e4c967',
-rock: '#cabb7b',
-fairy: '#eeb2fa',
-poison: '#9d619d',
-bug: '#c5cd4a',
-dragon: '#857af7',
-psychic: '#e56eaf',
-flying: '#80a4f9',
-fighting: '#9b5a48',
-normal: '#bab8ab'
+const fetchPokemon = async(number) => {
+    if (number === undefined) return;
+    const url = `https://pokeapi.co/api/v2/pokemon/${number}`;
+    const response = await fetch(url).then((response) => response.json());
+    const { id, name, types } = response;
+    const type = getType(types);
+    return { id, name, type };
 };
 
-const variables = [];
-const style = [];
-for (let key in colors) {
-
-variables.push(`--${key}: ${colors[key]};\n`);
-    
-    const css = `
-      .bg-${key} {
-          background: linear-gradient(to top right, var(--${key}), var(--card-bg) 25%);
-      }
-
-      .${key} {
-          background-color: var(--${key});
-      }
-    `;
-      style.push(css)
+const replacer = (text, source, destination) => {
+    const regex = new RegExp(source, 'gi');
+    return text.replace(regex, destination);
 }
-console.log(variables.join(''));
-console.log(style.join('\n'));
+
+const createPokemonCard = (pokemon) => {
+    const {id, name, type} = pokemon;
+    let newCard = replacer(cardHTML, `\{id\}`, id);
+    newCard = replacer(newCard, `\{name\}`, name);
+    newCard = replacer(newCard, `\{type\}`, type);
+
+    cards.innerHTML+= newCard;
+};
+
+const fetchPokemons = async() => {
+    for (let i = 1; i <= POKEMON_COUNT; i++) {
+        const pokemon = await fetchPokemon(i);
+        createPokemonCard(pokemon)
+    }
+};
+
+fetchPokemons(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cards = document.querySelector('.cards')
+// const card = document.querySelector('.card');
+
+// for(let i = 0; i < 30; i++) {
+//    const clone = card.cloneNode(true);
+//    cards.appendChild(clone)
+// }
